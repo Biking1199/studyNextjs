4e7d7ba82777e82c3fab48f701c78e9f5d896ff5
hibiki hangai <hibihibi99@gmail.com>
2024-10-22 21:03:17 +0900

    データフェッチング学習

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Link from "next/link";
 import styles from "../styles/Home.module.css";
+import { getSortedPostsData } from "../lib/posts";
 
 export default function Home() {
   return (
--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,11 +1,17 @@
+import Head from "next/head";
 import Link from "next/link";
+import Layout from "../../compenents/layout";
+
 export default function FirstPost() {
   return (
-    <>
+    <Layout>
+      <Head>
+        <title>hibiki Post</title>
+      </Head>
       <h1>First Post</h1>
       <h2>
         <Link href="/">Back to home</Link>
       </h2>
-    </>
+    </Layout>
   );
 }
